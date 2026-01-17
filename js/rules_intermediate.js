a2e8df162946ac061c542102b2dcc577d788af86
二叉树树 <acofork@qq.com>
2026-01-17 18:57:25 +0800

    Add short link: OgoRCAa

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2650,5 +2650,9 @@ window.RULES_INTERMEDIATE = {
     "/1SYTdAQ": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/OgoRCAa": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
