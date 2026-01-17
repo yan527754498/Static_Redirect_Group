f1e1c4a6f415bacce503a9deebd4ca3027b7abdf
二叉树树 <acofork@qq.com>
2026-01-17 18:56:01 +0800

    Add short link: iv96Gbz

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2466,5 +2466,9 @@ window.RULES_INTERMEDIATE = {
     "/VCszUF5": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/iv96Gbz": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
