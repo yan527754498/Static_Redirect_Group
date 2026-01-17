641e5644db706d7d19d929b6097c54789987931a
二叉树树 <acofork@qq.com>
2026-01-17 18:48:34 +0800

    Add short link: nVBLE0N

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2214,5 +2214,9 @@ window.RULES_INTERMEDIATE = {
     "/1piw0b3": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/nVBLE0N": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
