05de8ae9576bb3a774d116460fa356c401711349
二叉树树 <acofork@qq.com>
2026-01-17 18:50:06 +0800

    Add short link: kmZFXzv

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2342,5 +2342,9 @@ window.RULES_INTERMEDIATE = {
     "/XydKuvr": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/kmZFXzv": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
