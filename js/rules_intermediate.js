f0d481e3839b1dcaa32f6b906003367e9b174077
二叉树树 <acofork@qq.com>
2026-01-17 18:50:22 +0800

    Add short link: ERXiDxf

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2366,5 +2366,9 @@ window.RULES_INTERMEDIATE = {
     "/pV9Q1iR": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/ERXiDxf": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
