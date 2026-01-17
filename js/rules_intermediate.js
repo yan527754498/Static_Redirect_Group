b17a36a58c480789b5fc9f454079966538c94b46
二叉树树 <acofork@qq.com>
2026-01-17 18:48:58 +0800

    Add short link: DMnmIXu

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2250,5 +2250,9 @@ window.RULES_INTERMEDIATE = {
     "/R7gGHO6": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/DMnmIXu": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
