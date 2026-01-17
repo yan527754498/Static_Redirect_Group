71b25f3f71568ffe0d8d442f9c62ef8ba68557c0
二叉树树 <acofork@qq.com>
2026-01-17 18:57:21 +0800

    Add short link: 5IlFZJw

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2638,5 +2638,9 @@ window.RULES_INTERMEDIATE = {
     "/cE9JsGA": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/5IlFZJw": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
