c3ecc1830664b77409fd7f6bd548d08e44750631
二叉树树 <acofork@qq.com>
2026-01-21 19:49:15 +0800

    Add short link: ahf64

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -5098,5 +5098,9 @@ window.RULES_INTERMEDIATE = {
     "/l3qaqeg": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T11:49:13.000Z"
+    },
+    "/ahf64": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T11:49:15.000Z"
     }
 };
